599f34a8c97eb392fb8ce1e3da596b1e1d60b810
khai96_ <hvksmr1996@gmail.com>
2020-03-31 20:18:01 +0700

    Use assertEquals: Pretty diff message

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-import { assertStrictEq, assertEquals } from 'https://deno.land/std@v0.38.0/testing/asserts.ts'
+import { assertEquals } from 'https://deno.land/std@v0.38.0/testing/asserts.ts'
 import * as bundle from './bundle.js'
 import * as srcMod from './src/mod.ts'
 
 Deno.test('A is an object', () => {
-  assertStrictEq(typeof bundle.A, 'object')
+  assertEquals(typeof bundle.A, 'object')
 })
 
 Deno.test('B is an object', () => {
-  assertStrictEq(typeof bundle.B, 'object')
+  assertEquals(typeof bundle.B, 'object')
 })
 
 Deno.test('./bundle.js export that same object as ./src/mod.ts', () => {
