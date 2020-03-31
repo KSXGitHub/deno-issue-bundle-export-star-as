b2f8b6195654c63161b016169c98fc3236ad7f0b
khai96_ <hvksmr1996@gmail.com>
2020-03-31 20:15:02 +0700

    Convert to static import

--- test.js
+++ test.js
@@ -1,16 +1,15 @@
 import { assertStrictEq, assertEquals } from 'https://deno.land/std@v0.38.0/testing/asserts.ts'
-import { A, B } from './bundle.js'
+import * as bundle from './bundle.js'
+import * as srcMod from './src/mod.ts'
 
 Deno.test('A is an object', () => {
-  assertStrictEq(typeof A, 'object')
+  assertStrictEq(typeof bundle.A, 'object')
 })
 
 Deno.test('B is an object', () => {
-  assertStrictEq(typeof B, 'object')
+  assertStrictEq(typeof bundle.B, 'object')
 })
 
-Deno.test('./bundle.js export that same object as ./src/mod.ts', async () => {
-  const bundle = await import('./bundle.js')
-  const srcMod = await import('./src/mod.ts')
+Deno.test('./bundle.js export that same object as ./src/mod.ts', () => {
   assertEquals(bundle, srcMod)
 })
